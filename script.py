117ee3b563742146f771add45116edb0eb9da38e
Rebecca Ghidini <rebeccaghi2001@gmail.com>
2021-10-15 22:17:58 +0200

    Initialized streamlit application with file uploading feature

--- script.py
+++ script.py
@@ -1,14 +1,22 @@
 import wandb
+import streamlit as st
 
-wandb.login()
-# 1. Start a W&B run
-wandb.init(project='redensificaition', entity='redensificators')
+# wandb.login()
+# # 1. Start a W&B run
+# wandb.init(project='redensificaition', entity='redensificators')
 
-# 2. Save model inputs and hyperparameters
-config = wandb.config
-config.learning_rate = 0.01
+# # 2. Save model inputs and hyperparameters
+# config = wandb.config
+# config.learning_rate = 0.01
 
-# Model training here
+# # Model training here
 
-# 3. Log metrics over time to visualize performance
-wandb.log({"loss": 0.1})
+# # 3. Log metrics over time to visualize performance
+# wandb.log({"loss": 0.1})
+st.write("Hello world")
+
+
+uploaded_file = st.file_uploader("Upload Files",type=['png','jpeg']) 
+if uploaded_file is not None:
+    file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
+    st.write(file_details)
